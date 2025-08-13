ed06e02c900dabf324c7058e9b446012329496cf
Md. Salim Hosen <salimhosen2023@gmail.com>
2025-08-13 21:28:27 +0600

    update

--- src/Compunent/BannerSlick.jsx
+++ src/Compunent/BannerSlick.jsx
@@ -25,7 +25,6 @@ const BannerSlick = () => {
     var settings = {
         dots: true,
         infinite: true,
-        speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
